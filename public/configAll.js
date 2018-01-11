31cc71a64d2b48d483eeb613268d8514f8ee4952
fedehuguet <fede.huguet14@gmail.com>
2018-01-11 11:09:10 -0600

    Cambiar

--- public/configAll.js
+++ public/configAll.js
@@ -2,6 +2,17 @@ $( document ).ready(function() {
 	$(window).on('beforeunload', function() {
 		$(window).scrollTop(0);
 	});
+	var scroll_start = 0;
+   	var startchange = $('#startchange');
+   	var offset = startchange.offset();
+	$(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top + 200) {
+          $(".navbar-default").css('background-color', '#f0f0f0');
+       } else {
+          $('.navbar-default').css('background-color', 'transparent');
+       }
+   });
 
 	$(document).on('click', 'a[href^="#"]', function (event) {
 		event.preventDefault();
