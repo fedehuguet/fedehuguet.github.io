cf32e3011891de773bef253e10c8c7636391e2e3
fedehuguet <fede.huguet14@gmail.com>
2018-01-11 11:35:53 -0600

    Cambiar

--- public/configAll.js
+++ public/configAll.js
@@ -5,6 +5,7 @@ $( document ).ready(function() {
 	var scroll_start = 0;
    	var startchange = $('#startchange');
    	var offset = startchange.offset();
+   	$('.navbar').css('background-color', 'rgba(0, 0, 0, 0.3)');
 	$(document).scroll(function() { 
       scroll_start = $(this).scrollTop();
       if(scroll_start >= 250) {
